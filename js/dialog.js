557eb208588dbf83277989b1834f24a485fdeb2b
felipefbmendes <34218934+felipefbmendes@users.noreply.github.com>
2017-12-04 21:40:52 -0200

    dialog.js

---
+++ js/dialog.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+              $("#dialog").dialog({
+                  autoOpen: false,
+                  position: 'center' ,
+                  title: 'EDIT',
+                  draggable: false,
+                  width : 300,
+                  height : 40, 
+                  resizable : false,
+                  modal : true,
+              });
+
+              //$("a.delete-href").click( function() {
+              $('.conheca-ebook').on('click', function () {
+                //  $("#dialog").load('/form2.html', function() {
+                      $("#dialog").dialog("open");
+                 // });
+              })
+              
+});
