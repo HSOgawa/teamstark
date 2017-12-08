912493b841e68f722b74cbfb01d0566d7eb4368b
felipefbmendes <34218934+felipefbmendes@users.noreply.github.com>
2017-12-08 03:09:57 -0200

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -6,30 +6,41 @@ function sendRequest(form) {
         var email = document.getElementById("inputEmail").value;
         var categoria = $('input[name="categoria"]:checked').val();
     }
-    else {
+    else if (form == 2) {
         var name = document.getElementById("inputNome2").value;
         var email = document.getElementById("inputEmail2").value;
         var categoria = $('input[name="categoria2"]:checked').val();
+    }
+    else {
+        var name = document.getElementById("inputNome3").value;
+        var email = document.getElementById("inputEmail3").value;
+        var categoria = $('input[name="categoria3"]:checked').val();
     }
 
     if (name == "" || email == "" || !categoria) {
         if (name == "") {
             if (form == 1)
                 document.getElementById("nomeErro").textContent = "Por favor insira seu nome";
-            else
+            else if (form == 2)
                 document.getElementById("nomeErro2").textContent = "Por favor insira seu nome";
+            else
+                document.getElementById("nomeErro3").textContent = "Por favor insira seu nome";
         }
         if (email == "") {
             if (form == 1)
                 document.getElementById("emailErro").textContent = "Por favor insira seu e-mail";
-            else
+            else if (form == 2)
                 document.getElementById("emailErro2").textContent = "Por favor insira seu e-mail";
+            else
+                document.getElementById("emailErro3").textContent = "Por favor insira seu e-mail";
         }
         if (!categoria) {
             if (form == 1)
                 document.getElementById("categoriaErro").textContent = "Por favor selecione uma opção";
-            else
+            else if (form == 2)
                 document.getElementById("categoriaErro2").textContent = "Por favor selecione uma opção";
+            else
+                document.getElementById("categoriaErro3").textContent = "Por favor selecione uma opção";
         }
 
     }
@@ -46,10 +57,15 @@ function sendRequest(form) {
             document.getElementById("inputEmail").value = "";
             $('input[name="categoria"]').prop('checked', false);
         }
-        else {
+        else if (form == 2) {
             document.getElementById("inputNome2").value = "";
             document.getElementById("inputEmail2").value = "";
             $('input[name="categoria2"]').prop('checked', false);
+        }
+        else {
+            document.getElementById("inputNome3").value = "";
+            document.getElementById("inputEmail3").value = "";
+            $('input[name="categoria3"]').prop('checked', false);
         }
     }
     return false;
@@ -61,9 +77,14 @@ function Reset(form) {
         document.getElementById("emailErro").textContent = "";
         document.getElementById("categoriaErro").textContent = "";
     }
-    else {
+    else if (form == 2) {
         document.getElementById("nomeErro2").textContent = "";
         document.getElementById("emailErro2").textContent = "";
         document.getElementById("categoriaErro2").textContent = "";
+    }
+    else {
+        document.getElementById("nomeErro3").textContent = "";
+        document.getElementById("emailErro3").textContent = "";
+        document.getElementById("categoriaErro3").textContent = "";
     }
 }
